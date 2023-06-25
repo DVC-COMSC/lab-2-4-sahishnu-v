61bcfe8c7c081735308b67115e078b76279f808b
sahishnu-v <sahishnuvenkatesan@gmail.com>
2023-06-24 21:57:27 -0700

    edit main.py

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_str = 'Python Programming'
+
+    sub1 = original_str[0:7]
+    sub2 = original_str[7:18]
+    merged_str = sub2 +" "+ sub1
+    
+    print (sub1)
+    print (sub2)
+    print (merged_str)
     pass
 
 
